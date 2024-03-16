6b5737461909deac2288e4d79f64b500e3d45b1a
Tigran Farmanyan <tigran.farmanyan@gmail.com>
2024-03-16 18:44:19 -0500

    Add metadata for page titles

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,6 +8,12 @@ import {
   RevenueChartSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Overview',
+};
+
 
 export default async function Page() {
 
